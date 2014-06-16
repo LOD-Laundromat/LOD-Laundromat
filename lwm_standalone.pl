bad532e77af000c9d7b2cb6c66d595bdb816f463
Wouter Beek <me@wouterbeek.com>
2014-06-16 19:51:36 +0200

    Load debug monitor.

--- lwm_standalone.pl
+++ lwm_standalone.pl
@@ -6,6 +6,10 @@
 :- ensure_loaded(debug).
 :- ensure_loaded(load).
 
+:- use_module(library(swi_ide)).
+:- prolog_ide(debug_monitor).
+:- debug(sparql_api).
+
 :- use_module(lwm(lwm_start)).
 :- run_washing_machine.
 
