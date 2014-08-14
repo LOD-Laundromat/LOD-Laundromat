fc4b0c1d197f2d5f1726d426bff5baf31bfd5a67
Wouter Beek <me@wouterbeek.com>
2014-08-14 11:02:07 +0200

    * [SC] Removed version object.

--- run_singlethread.pl
+++ run_singlethread.pl
@@ -39,9 +39,6 @@ run_singlethread:-
 %! ) .
 
 run_singlethread(NumberOfUnpackLoops, NumberOfCleanLoops):-
-  % Make sure the LOD Washing Machine version object is sent to
-  % the LOD Laundromat Endpoint.
-  init_lwm_version,
   forall(
     between(1, NumberOfUnpackLoops, _),
     thread_create(lwm_unpack_loop, _, [detached(true)])
@@ -50,37 +47,3 @@ run_singlethread(NumberOfUnpackLoops, NumberOfCleanLoops):-
     between(1, NumberOfCleanLoops, _),
     thread_create(lwm_clean_loop, _, [detached(true)])
   ).
-
-
-%! init_lwm_version is det.
-% Initializes the current version of the LOD Washing Machine
-% at the LOD Laundromat Endpoint.
-%
-% A LOD Washing Machine version is represented by a resource with
-%  a version number,
-%  a start dataTime,
-%  and a data directory.
-
-init_lwm_version:-
-  lwm_version_object(Version),
-  
-  % IS-A.
-  store_triple(Version, rdf-type, ll-'Version'),
-  
-  % Label.
-  lwm_version_number(VersionNumber),
-  format(atom(Label), 'LOD Washing Machine version ~D.', [VersionNumber]),
-  store_triple(Version, rdfs-label, literal(type(xsd-string,Label))),
-  
-  % dataTime.
-  get_dateTime(Now),
-  store_triple(Version, ll-start, literal(type(xsd-dateTime,Now))),
-  
-  % Version directory.
-  lwm_version_directory(VersionDir),
-  uri_file_name(VersionUri, VersionDir),
-  store_triple(Version, ll-datadir, VersionUri),
-  
-  % Send SPARQL Update request.
-  post_rdf_triples.
-
