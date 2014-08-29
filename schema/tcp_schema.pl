35806318370f57b03566546371a0d5771457181d
Wouter Beek <w.g.j.beek@vu.nl>
2014-08-29 07:54:21 +0200

    Added TCP error codes.

--- schema/tcp_schema.pl
+++ schema/tcp_schema.pl
@@ -52,32 +52,41 @@ tcp_error('EADDRNOTAVAIL', 'Cannot assign requested address').
 tcp_error('EAFNOSUPPORT', 'Address family not supported by protocol').
 tcp_error('EALREADY', 'Operation already in progress').
 tcp_error('EBADF', 'Bad file descriptor').
+tcp_error('EBADSLT', 'Invalid slot').
 tcp_error('ECONNABORTED', 'Software caused connection abort').
 tcp_error('ECONNREFUSED', 'Connection refused').
 tcp_error('ECONNRESET', 'Connection reset by peer').
+tcp_error('EDQUOT', 'Disk quota exceeded').
 tcp_error('EHOSTDOWN', 'Host is down').
 tcp_error('EHOSTUNREACH', 'No route to host').
 tcp_error('EINPROGRESS', 'Operation now in progress').
 tcp_error('EINTR', 'Interrupted system call').
 tcp_error('EIO', 'Input/output error').
 tcp_error('EISCONN', 'Transport endpoint is already connected').
+tcp_error('EINVAL', 'Invalid argument').
 tcp_error('ELOOP', 'Too many levels of symbolic links').
-tcp_error('ENAMETOOLONG', 'File name too long').
+tcp_error('EMSGSIZE', 'Message too long').
 tcp_error('ENETDOWN', 'Network is down').
 tcp_error('ENETRESET', 'Network dropped connection on reset').
 tcp_error('ENETUNREACH', 'Network is unreachable').
+tcp_error('ENOBUFS', 'No buffer space available').
+tcp_error('ENODATA', 'No data available').
 tcp_error('ENOENT', 'No such file or directory').
+tcp_error('ENOTCONN', 'Transport endpoint is not connected').
 tcp_error('ENOTDIR', 'Not a directory').
 tcp_error('ENOTSOCK', 'Socket operation on non-socket').
-tcp_error('EPROTOTYPE', 'Protocol wrong type for socket').
-tcp_error('ETIMEDOUT', 'Connection timed out').
-tcp_error('EINVAL', 'Invalid argument').
-tcp_error('ELOOP', 'Too many levels of symbolic links').
-tcp_error('ENAMETOOLONG', 'File name too long').
-tcp_error('ENETDOWN', 'Network is down').
-tcp_error('ENOBUFS', 'No buffer space available').
 tcp_error('EOPNOTSUPP', 'Operation not supported').
+tcp_error('EPFNOSUPPORT', 'Protocol family not supported').
 tcp_error('EPIPE', 'Broken pipe').
+tcp_error('EPROTO', 'Protocol error').
+tcp_error('EPROTONOSUPPORT', 'Protocol not supported').
+tcp_error('EPROTOTYPE', 'Protocol wrong type for socket').
+tcp_error('EREMOTE', 'Object is remote').
 tcp_error('EREMOTEIO', 'Remote I/O error').
+tcp_error('ESHUTDOWN', 'Cannot send after transport endpoint shutdown').
+tcp_error('ESTALE', 'Stale file handle').
+tcp_error('ESOCKTNOSUPPORT', 'Socket type not supported').
 tcp_error('ETIMEDOUT', 'Connection timed out').
-
+tcp_error('ETOOMANYREFS', 'Too many references: cannot splice').
+tcp_error('EUSERS', 'Too many users').
+tcp_error('EWOULDBLOCK', 'Resource temporarily unavailable').
