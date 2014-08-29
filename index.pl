ec0ae6ddfa8589ec613708fddffe055ca910b211
Wouter Beek <me@wouterbeek.com>
2014-08-29 13:12:51 +0200

    * [SC] Moved ontology generating scripts to a separate repo.

--- index.pl
+++ index.pl
@@ -2,4 +2,3 @@
 % This assumes that file search path `lwm` has been set.
 
 user:file_search_path(lwm_basket, lwm(basket)).
-user:file_search_path(lwm_schema, lwm(schema)).
--- lwm_store_triple.pl
+++ lwm_store_triple.pl
@@ -52,7 +52,6 @@ the stored triples are sent in a SPARQL Update request
 :- use_module(plXsd_datetime(xsd_dateTime_ext)).
 
 :- use_module(lwm(noRdf_store)).
-:- use_module(lwm_schema(schema_tcp)).
 
 :- rdf_register_prefix(error, 'http://lodlaundromat.org/error/ontology/').
 :- rdf_register_prefix(http, 'http://lodlaundromat.org/http/ontology/').
@@ -332,4 +331,53 @@ number_of_duplicates_written(T) --> [' (~D dups)'-[T]].
 
 number_of_triples_written(0) --> !, [].
 number_of_triples_written(T) --> ['+~D'-[T]].
+
+
+
+% Helpers.
+
+tcp_error('EACCES', 'Permission denied').
+tcp_error('EADDRINUSE', 'Address already in use').
+tcp_error('EADDRNOTAVAIL', 'Cannot assign requested address').
+tcp_error('EAFNOSUPPORT', 'Address family not supported by protocol').
+tcp_error('EALREADY', 'Operation already in progress').
+tcp_error('EBADF', 'Bad file descriptor').
+tcp_error('EBADSLT', 'Invalid slot').
+tcp_error('ECONNABORTED', 'Software caused connection abort').
+tcp_error('ECONNREFUSED', 'Connection refused').
+tcp_error('ECONNRESET', 'Connection reset by peer').
+tcp_error('EDQUOT', 'Disk quota exceeded').
+tcp_error('EHOSTDOWN', 'Host is down').
+tcp_error('EHOSTUNREACH', 'No route to host').
+tcp_error('EINPROGRESS', 'Operation now in progress').
+tcp_error('EINTR', 'Interrupted system call').
+tcp_error('EIO', 'Input/output error').
+tcp_error('EISCONN', 'Transport endpoint is already connected').
+tcp_error('EINVAL', 'Invalid argument').
+tcp_error('ELOOP', 'Too many levels of symbolic links').
+tcp_error('EMSGSIZE', 'Message too long').
+tcp_error('ENETDOWN', 'Network is down').
+tcp_error('ENETRESET', 'Network dropped connection on reset').
+tcp_error('ENETUNREACH', 'Network is unreachable').
+tcp_error('ENOBUFS', 'No buffer space available').
+tcp_error('ENODATA', 'No data available').
+tcp_error('ENOENT', 'No such file or directory').
+tcp_error('ENOTCONN', 'Transport endpoint is not connected').
+tcp_error('ENOTDIR', 'Not a directory').
+tcp_error('ENOTSOCK', 'Socket operation on non-socket').
+tcp_error('EOPNOTSUPP', 'Operation not supported').
+tcp_error('EPFNOSUPPORT', 'Protocol family not supported').
+tcp_error('EPIPE', 'Broken pipe').
+tcp_error('EPROTO', 'Protocol error').
+tcp_error('EPROTONOSUPPORT', 'Protocol not supported').
+tcp_error('EPROTOTYPE', 'Protocol wrong type for socket').
+tcp_error('EREMOTE', 'Object is remote').
+tcp_error('EREMOTEIO', 'Remote I/O error').
+tcp_error('ESHUTDOWN', 'Cannot send after transport endpoint shutdown').
+tcp_error('ESTALE', 'Stale file handle').
+tcp_error('ESOCKTNOSUPPORT', 'Socket type not supported').
+tcp_error('ETIMEDOUT', 'Connection timed out').
+tcp_error('ETOOMANYREFS', 'Too many references: cannot splice').
+tcp_error('EUSERS', 'Too many users').
+tcp_error('EWOULDBLOCK', 'Resource temporarily unavailable').
 
