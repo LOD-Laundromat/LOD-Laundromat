55b5fd6bd31c7b04bb598e837fe9d7d85e9176ca
Wouter Beek <me@wouterbeek.com>
2014-10-22 13:10:35 +0200

    CLEANUP: Moved RDF accept header value to plRdf.

--- lwm_unpack.pl
+++ lwm_unpack.pl
@@ -143,7 +143,7 @@ unpack_datadoc(Md5, Datadoc, DirtyUrl):-
   directory_file_path(Md5Dir, LocalDownloadFile, DownloadFile),
 
   % Download the dirty file for the given Md5.
-  lod_accept_header_value(AcceptValue),
+  rdf_accept_header_value(AcceptValue),
   download_to_file(
     DirtyUrl,
     DownloadFile,
@@ -187,59 +187,55 @@ unpack_file(Md5, Datadoc, ArchiveFile):-
   store_archive_filters(Datadoc, ArchiveFilters),
 
   md5_directory(Md5, Md5Dir),
-  (
-    EntryPairs == []
-  ->
-    % There is no file for cleaning.
-    % To keep the process simple / consistent with other cases,
-    % we create an empty dirty file.
-    directory_file_path(Md5Dir, dirty, DirtyFile),
-    touch_file(DirtyFile)
+  (   EntryPairs == []
+  ->  % There is no file for cleaning.
+      % To keep the process simple / consistent with other cases,
+      % we create an empty dirty file.
+      directory_file_path(Md5Dir, dirty, DirtyFile),
+      touch_file(DirtyFile)
+  ;   % Exactly one raw file.
+      % This file is completely unarchived
+      % and should be moved to the cleaning phase.
+      EntryPairs = [data-EntryProperties],
+      memberchk(format(raw), EntryProperties)
+  ->  % Construct the data file name.
+      file_directory_name(ArchiveFile, ArchiveDir),
+      directory_file_path(ArchiveDir, data, DataFile),
+      
+      % Construct the dirty file name.
+      directory_file_path(Md5Dir, dirty, DirtyFile),
+      
+      % Move the data file outside of the its entry path,
+      % and put it directly inside its MD5 directory.
+      mv2(DataFile, DirtyFile),
+      size_file(DirtyFile, UnpackedSize),
+      store_triple(
+        Datadoc,
+        llo-unpackedSize,
+        literal(type(xsd-nonNegativeInteger,UnpackedSize))
+      )
+      % The file is now ready for cleaning!
   ;
-    % Exactly one raw file.
-    % This file is completely unarchived
-    % and should be moved to the cleaning phase.
-    EntryPairs = [data-EntryProperties],
-    memberchk(format(raw), EntryProperties)
-  ->
-    % Construct the data file name.
-    file_directory_name(ArchiveFile, ArchiveDir),
-    directory_file_path(ArchiveDir, data, DataFile),
-
-    % Construct the dirty file name.
-    directory_file_path(Md5Dir, dirty, DirtyFile),
-
-    % Move the data file outside of the its entry path,
-    % and put it directly inside its MD5 directory.
-    mv2(DataFile, DirtyFile),
-    size_file(DirtyFile, UnpackedSize),
-    store_triple(
-      Datadoc,
-      llo-unpackedSize,
-      literal(type(xsd-nonNegativeInteger,UnpackedSize))
-    )
-    % The file is now ready for cleaning!
-  ;
-    % Store the archive entries for future processing.
-    pairs_keys_values(EntryPairs, EntryPaths, EntryProperties1),
-
-    % Store the archive format.
-    filter_archive_formats(
-      EntryProperties1,
-      ArchiveFormats,
-      EntryProperties2
-    ),
-    distill_archive_format(ArchiveFormats, ArchiveFormat),
-    store_triple(
-      Datadoc,
-      llo-archiveFormat,
-      literal(type(xsd-string,ArchiveFormat))
-    ),
-
-    maplist(store_archive_entry(Md5, Datadoc), EntryPaths, EntryProperties2),
-    store_skip_clean(Md5, Datadoc)
+      % Store the archive entries for future processing.
+      pairs_keys_values(EntryPairs, EntryPaths, EntryProperties1),
+      
+      % Store the archive format.
+      filter_archive_formats(
+        EntryProperties1,
+        ArchiveFormats,
+        EntryProperties2
+      ),
+      distill_archive_format(ArchiveFormats, ArchiveFormat),
+      store_triple(
+        Datadoc,
+        llo-archiveFormat,
+        literal(type(xsd-string,ArchiveFormat))
+      ),
+      
+      maplist(store_archive_entry(Md5, Datadoc), EntryPaths, EntryProperties2),
+      store_skip_clean(Md5, Datadoc)
   ),
-
+  
   % Remove the archive file.
   delete_file(ArchiveFile).
 
@@ -267,50 +263,6 @@ filter_archive_formats([L1|Ls1], Fs1, [L2|Ls2]):-
   selectchk(format(F), L1, L2),
   filter_archive_formats(Ls1, Fs2, Ls2),
   ord_add_element(Fs2, F, Fs1).
-
-
-%! lod_accept_header_value(-Value:atom) is det.
-
-lod_accept_header_value(Value):-
-  findall(
-    Value,
-    (
-      lod_content_type(ContentType, Q),
-      format(atom(Value), '~a; q=~1f', [ContentType,Q])
-    ),
-    Values
-  ),
-  atomic_list_concat(Values, ', ', Value).
-
-
-%! lod_content_type(?ContentType:atom, ?QValue:between(0.0,1.0)) is nondet.
-
-% RDFa
-lod_content_type('text/html',              0.3).
-% N-Quads
-lod_content_type('application/n-quads',    0.8).
-% N-Triples
-lod_content_type('application/n-triples',  0.8).
-% RDF/XML
-lod_content_type('application/rdf+xml',    0.7).
-lod_content_type('text/rdf+xml',           0.7).
-lod_content_type('application/xhtml+xml',  0.3).
-lod_content_type('application/xml',        0.3).
-lod_content_type('text/xml',               0.3).
-lod_content_type('application/rss+xml',    0.5).
-% Trig
-lod_content_type('application/trig',       0.8).
-lod_content_type('application/x-trig',     0.5).
-% Turtle
-lod_content_type('text/turtle',            0.9).
-lod_content_type('application/x-turtle',   0.5).
-lod_content_type('application/turtle',     0.5).
-lod_content_type('application/rdf+turtle', 0.5).
-% N3
-lod_content_type('text/n3',                0.8).
-lod_content_type('text/rdf+n3',            0.5).
-% All
-lod_content_type('*/*',                    0.1).
 
 
 pair_to_triple(S, [P,O], rdf(S,P,O)).
