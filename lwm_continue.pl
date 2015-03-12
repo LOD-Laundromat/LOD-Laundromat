78a37cd322a347eef7ebf741c915bc8d342d8824
Wouter Beek <w.g.j.beek@vu.nl>
2015-03-12 07:39:21 +0100

    DEBUG: MD5 -> Datadoc.

--- lwm_continue.pl
+++ lwm_continue.pl
@@ -10,7 +10,7 @@
 Continues an interrupted LOD Washing Machine crawl.
 
 @author Wouter Beek
-@version 2014/09, 2015/01-2015/02
+@version 2014/09, 2015/01-2015/03
 */
 
 :- use_module(library(apply)).
@@ -21,7 +21,6 @@ Continues an interrupted LOD Washing Machine crawl.
 :- use_module(plc(process/list_script)).
 
 :- use_module(lwm(lwm_reset)).
-:- use_module(lwm(query/lwm_sparql_generics)).
 :- use_module(lwm(query/lwm_sparql_query)).
 
 
@@ -78,31 +77,37 @@ erroneous_datadocs0(L):-
     []
   ).
 */
-/*
 % fedBench
-erroneous_datadocs0([
-  '6c7100daab8122f770ae9feaa45fc47c',
-  'd1e641a73d97429e3fe5cd5e2d8dd37c',
-  '03f6f715d4339d7a62d0b693634f1cbc',
-  '7d24b8cc9fb5255d8d53dee3138c56d3',
-  '1c4c8920a6e136d6bf2f050d31d78cd2',
-  '6c5e3bba18d848704c882f23f21813fd',
-  '69801d94cb75c15bfda0d8265b6e7d75',
-  'd11b99e99ca29ec3dd4231f4681a3b7b',
-  '31d1024d3cfbdb30358c208e8b09ba64',
-  'f4285af57cb8152a874fdf611bf875f9',
-  'b5075134338585f5f5ff47299a8bda30',
-  '65a2b25a4df5b593ce1c4a7a80d7b5b6',
-  'c26458fd28f02e90292e60565d2ce427',
-  '4fbd5c29adbbaba306fcdf1f9012556e',
-  '51631129c96af54f69792459a6395ee0',
-  '6108b9db4f7ffe62ad52d62c9bc2e3db',
-  'c951af2cd6bd89917c0ff08185e0db33',
-  '0a54e7b9edc759c8d78d15e649c39dd1',
-  'd1153876a11e0ff78023ab43e708c843',
-  '69bac2bdcf003789787e6ac0a0a292a4',
-  '7c034cdabbe0d82eda31ea22d353d23e',
-  'a6c84464231c829eeb951dd9e69634d5',
-  '65c8fe18943bfb26667c8d428f3d52a5'
-]).
-*/
+erroneous_datadocs0(Datadocs):-
+  findall(
+    Datadoc,
+    (
+      erroneous_md5(Md5),
+      rdf_global_id(ll:Md5, Datadoc)
+    ),
+    Datadocs
+  ).
+
+erroneous_md5('6c7100daab8122f770ae9feaa45fc47c').
+erroneous_md5('d1e641a73d97429e3fe5cd5e2d8dd37c').
+erroneous_md5('03f6f715d4339d7a62d0b693634f1cbc').
+erroneous_md5('7d24b8cc9fb5255d8d53dee3138c56d3').
+erroneous_md5('1c4c8920a6e136d6bf2f050d31d78cd2').
+erroneous_md5('6c5e3bba18d848704c882f23f21813fd').
+erroneous_md5('69801d94cb75c15bfda0d8265b6e7d75').
+erroneous_md5('d11b99e99ca29ec3dd4231f4681a3b7b').
+erroneous_md5('31d1024d3cfbdb30358c208e8b09ba64').
+erroneous_md5('f4285af57cb8152a874fdf611bf875f9').
+erroneous_md5('b5075134338585f5f5ff47299a8bda30').
+erroneous_md5('65a2b25a4df5b593ce1c4a7a80d7b5b6').
+erroneous_md5('c26458fd28f02e90292e60565d2ce427').
+erroneous_md5('4fbd5c29adbbaba306fcdf1f9012556e').
+erroneous_md5('51631129c96af54f69792459a6395ee0').
+erroneous_md5('6108b9db4f7ffe62ad52d62c9bc2e3db').
+erroneous_md5('c951af2cd6bd89917c0ff08185e0db33').
+erroneous_md5('0a54e7b9edc759c8d78d15e649c39dd1').
+erroneous_md5('d1153876a11e0ff78023ab43e708c843').
+erroneous_md5('69bac2bdcf003789787e6ac0a0a292a4').
+erroneous_md5('7c034cdabbe0d82eda31ea22d353d23e').
+erroneous_md5('a6c84464231c829eeb951dd9e69634d5').
+erroneous_md5('65c8fe18943bfb26667c8d428f3d52a5').
