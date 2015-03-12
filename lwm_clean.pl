0a250c594503ed88918f6e6fb7ead97f6d704aa1
Wouter Beek <w.g.j.beek@vu.nl>
2015-03-12 08:39:59 +0100

    DEPLOYMENT: Sleep longer.

--- lwm_clean.pl
+++ lwm_clean.pl
@@ -82,7 +82,7 @@ lwm_clean_loop(Category, Min, Max):-
   lwm_clean_loop(Category, Min, Max).
 % Done for now. Check whether there are new jobs in one seconds.
 lwm_clean_loop(Category, Min, Max):-
-  sleep(30),
+  sleep(100),
   lwm_debug_message(lwm_idle_loop(Category)), % DEB
   lwm_clean_loop(Category, Min, Max).
 
--- lwm_unpack.pl
+++ lwm_unpack.pl
@@ -68,7 +68,7 @@ lwm_unpack_loop:-
   lwm_unpack_loop.
 % Done for now. Check whether there are new jobs in one seconds.
 lwm_unpack_loop:-
-  sleep(30),
+  sleep(100),
   lwm_debug_message(lwm_idle_loop(unpack)), % DEB
   lwm_unpack_loop.
 
