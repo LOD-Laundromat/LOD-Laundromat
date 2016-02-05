026c0251bc16225f3b8d640c37f74d5ca2488856
Wouter Beek <me@wouterbeek.com>
2016-02-05 10:56:17 +0100

    It is too difficult to extract the llo:added property so neglect it.

--- old_seedlist.pl
+++ old_seedlist.pl
@@ -3,7 +3,7 @@
 :- use_module(library(debug)).
 :- use_module(library(lists)).
 :- use_module(library(os/gnu_sort)).
-:- use_module(library(rdf11/rdf11)).
+:- use_module(library(rdf11/rdf11)). % Operators.
 :- use_module(library(sparql/sparql_db)).
 :- use_module(library(sparql/query/sparql_query)).
 
@@ -21,24 +21,18 @@ run :-
   absolute_file_name(seedlist, File, [access(write),file_type(prolog)]),
   Q = '\c
 PREFIX llo: <http://lodlaundromat.org/ontology/>\n\c
-SELECT ?url ?added\n\c
+SELECT ?url\n\c
 WHERE {\n\c
-  ?doc llo:url ?url ;\n\c
-       llo:added ?added\n\c
+  ?doc llo:url ?url\n\c
 }\n',
+  get_time(Now),
   setup_call_cleanup(
     open(File, write, Write),
     forall(
       sparql_select(ll_endpoint, Q, Rows),
-      forall(member([Url,Added], Rows), assert_seed(Write, Url, Added))
+      forall(member([Url], Rows),   format(Write, 'seed(\'~a\',~w,0.0,0.0)~n', [Url,Now]))
     ),
     close(Write)
   ),
   sort_file(File),
   halt.
-
-assert_seed(Write, Url, Added1) :-
-  gtrace,
-  rdf11:post_object(Added2, Added1),
-  date_time_stamp(Added2, Added3),
-  format(Write, 'seed(~w,~w,0.0,0.0)~n', [Url,Added3]).
