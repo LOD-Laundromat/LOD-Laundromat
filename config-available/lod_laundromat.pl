8fc49273535cba2c65bca27cbc9d22cbe29ef1be
Wouter Beek <me@wouterbeek.com>
2016-02-23 00:15:48 +0100

    ADDED: Image path for Web resource serving.

--- config-available/lod_laundromat.pl
+++ config-available/lod_laundromat.pl
@@ -8,6 +8,15 @@
 :- set_setting(http:public_host, 'cliopatria.lod.labs.vu.nl').
 :- set_setting(http:public_scheme, http).
 
+:- dynamic
+    user:file_search_path/2.
+
+:- multifile
+    user:file_search_path/2.
+
+user:file_search_path(web, cpack('LOD-Laundromat'/web)).
+user:file_search_path(img, web(img)).
+
 :- use_module(cpack('LOD-Laundromat'/lod_basket)).
 :- use_module(cpack('LOD-Laundromat'/lod_laundromat)).
 
