9204230e9a2d2756b3962db72b417814dfa0c4fa
Wouter Beek <me@wouterbeek.com>
2016-03-17 17:24:43 +0100

    Moving back very specific code from plRdf.

--- washing_machine.pl
+++ washing_machine.pl
@@ -19,13 +19,19 @@
 :- use_module(library(apply)).
 :- use_module(library(debug_ext)).
 :- use_module(library(filesex)).
+:- use_module(library(gen/gen_ntuples)).
 :- use_module(library(hash_ext)).
+:- use_module(library(http/json)).
+:- use_module(library(jsonld/jsonld_metadata)).
+:- use_module(library(jsonld/jsonld_read)).
 :- use_module(library(msg_ext)).
 :- use_module(library(os/open_any2)).
 :- use_module(library(os/thread_counter)).
 :- use_module(library(os/thread_ext)).
+:- use_module(library(pl/pl_term)).
 :- use_module(library(rdf/rdf_clean)).
 :- use_module(library(rdf/rdf_debug)).
+:- use_module(library(rdf/rdf_ext)).
 :- use_module(library(rdf/rdf_load)).
 :- use_module(library(semweb/rdf11)). % Operators.
 :- use_module(library(string_ext)).
@@ -34,6 +40,12 @@
 
 :- use_module(cpack('LOD-Laundromat'/laundromat_fs)).
 :- use_module(cpack('LOD-Laundromat'/seedlist)).
+
+:- meta_predicate
+    rdf_store_messages(+, +, 0).
+
+:- rdf_meta
+   rdf_store_messages(+, r, :).
 
 :- dynamic
     currently_debugging0/1.
@@ -99,9 +111,9 @@ clean0(Hash, Iri) :-
       open_any2(MetaFile, append, _, MetaClose_0, [alias(MetaAlias)|FileOpts]),
       open_any2(WarnFile, append, _, WarnClose_0, [alias(WarnAlias)|FileOpts])
     ),
-    rdf_store_messages(Doc, (
+    rdf_store_messages(WarnAlias, Doc, (
       rdf_clean(Iri, DataFile, CleanOpts),
-      rdf_store_metadata(Doc, M)
+      rdf_store_metadata(MetaAlias, Doc, M)
     )),
     (
       close_any2(WarnClose_0),
@@ -170,3 +182,63 @@ washing_machine0 :-
   S is M * N,
   debug(washing_machine(idle), "Washing machine ~w is ~D sec. idle", [Name,S]),
   washing_machine0.
+
+
+
+%! rdf_store_messages(+Output, +S, :Goal_0) is det.
+% Run Goal, unify Result with `true`, `false` or `exception(Error)`
+% and messages with a list of generated error and warning messages.
+% Each message is a term `message(Term,Kind,Lines)`.
+
+rdf_store_messages(Output, S, Goal_0) :-
+  setup_call_cleanup(
+    (
+      create_thread_counter(rdf_warning),
+      asserta((
+        user:thread_message_hook(Term,Kind,_) :-
+          error_kind(Kind),
+          threadsafe_format(warn, "~w~n", [Term]),
+          increment_thread_counter(rdf_warning)
+      ))
+    ),
+    (
+      (   catch(Goal_0, E, true)
+      ->  (   var(E)
+          ->  Result = true,
+              End0 = true
+          ;   E = error(existence_error(open_any2,M),_)
+          ->  rdf_store_metadata(Output, S, M),
+              End0 = "No stream"
+          ;   Result = exception(E),
+              End0 = E
+          ),
+          debug(rdf(debug), "[RESULT] ~w ~w", [Result,Goal_0])
+      ;   msg_warning("[FAILED] ~w", [Goal_0]),
+          End0 = fail
+      ),
+      with_output_to(string(End), write_term(End0)),
+      with_output_to(Output,
+        gen_ntriple(S, llo:end, End^^xsd:string))
+    ),
+    (
+      delete_thread_counter(rdf_warning, N),
+      with_output_to(Output,
+        gen_ntriple(S, llo:number_of_warnings, N^^xsd:nonNegativeInteger))
+    )
+  ).
+
+error_kind(warning).
+error_kind(error).
+
+
+%! rdf_store_metadata(+Output, +S, +M) is det.
+
+rdf_store_metadata(Output, S1, M) :-
+  jsonld_metadata(M, Jsonld1),
+  atom_string(S1, S2),
+  Jsonld2 = Jsonld1.put(_{'@id': S2}),
+  (debugging(rdf(debug)) -> json_write_dict(user_output, Jsonld2) ; true),
+  forall(jsonld_tuple(Jsonld2, rdf(S,P,O)), (
+    (debugging(rdf(debug)) -> rdf_print(S, P, O, _) ; true),
+    with_output_to(Output, gen_ntriple(S, P, O))
+  )).
