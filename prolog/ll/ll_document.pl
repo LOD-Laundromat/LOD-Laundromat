8fc431c77225f68161d2d38a2bd796d506a29382
Wouter Beek <wouter@triply.cc>
2018-03-26 23:40:53 +0200

    ENHANCED: Updated known errors.

--- prolog/ll/ll_document.pl
+++ prolog/ll/ll_document.pl
@@ -45,7 +45,8 @@
 ll_document(Hash, Dir, Uri0) :-
   % Make sure that non-ASCII Unicode characters are percent encoded.
   uri_normalized(Uri0, Uri),
-  http_open2(Uri, In, [failure(-1),metadata(Metas)]),
+  % TBD: Assert (HTTP) metadata into LOD-Seedlist.
+  http_open2(Uri, In, [failure(-1),metadata(Metas),success('2xx')]),
   call_cleanup(
     (
       % Use a dummy value in case the Media Type cannot be determined.
--- script/ll_analyze.pl
+++ script/ll_analyze.pl
@@ -3,6 +3,7 @@
   [
     empty_dataset/2, % -Dataset, -Dict
     error/1,         % -Error
+    print_errors/0,
     print_status/0,
     unknown_error/1  % -Error
   ]
@@ -70,61 +71,24 @@ error_(Dataset-Error, In) :-
 
 
 
-%! unknown_error(-Error:pair(atom,compound)) is nondet.
+%! print_errors is det.
 
-unknown_error(Error) :-
-  error(Error),
-  Error = _-Error0,
-  \+ known_error_(Error0).
-
-%known_error_(error(domain_error(set_cookie,_Value),_)).
-%%%%known_error_(error(domain_error(url,_),_)). % TBD: ?
-%known_error_(error(existence_error(turtle_prefix,_Prefix),stream(_Stream,_X,_Y,_Z))).
-known_error_(error(http_status(400,_Msg),_Uri)).
-known_error_(error(http_status(404,_Msg),_Uri)).
-known_error_(error(http_status(406,_Msg),_Uri)).
-known_error_(error(http_status(500,_Msg),_Uri)).
-%%%%known_error_(error(io_error(read,_Stream),_Context)). % TBD: ?
-known_error_(error(socket_error('Connection timed out'),_)).
-known_error_(error(socket_error('Host not found'),_)).
-known_error_(error(syntax_error('EOF in string'),_Stream)).
-known_error_(error(syntax_error('Illegal character in uriref'),_Stream)).
-known_error_(error(syntax_error('Illegal control character in uriref'),_Stream)).
-known_error_(error(syntax_error('illegal escape'),_Stream)).
-%known_error_(error(timeout_error(read, _Stream),_Context)).
-%known_error_(http(max_redirect(_N,_Uris))).
-%known_error_(http(no_content_type, _Uri)).
-%known_error_(http(redirect_loop(_Uri))).
-known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#date',_)).
-known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#dateTime',_)).
-%known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#decimal',_)).
-known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#double',_)).
-%known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#float',_)).
-%known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#gDay',_)).
-%known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#gMonth',_)).
-%known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#gMonthDay',_)).
-%known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#gYear',_)).
-known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#int',_)).
-%known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#integer',_)).
-%known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#nonNegativeInteger',_)).
-%known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#time',_)).
-%known_error_(missing_language_tag(_LTag)).
-%known_error_(non_canonical_language_tag(_LTag)).
-%known_error_(no_image(_Url)).
-known_error_(non_canonical_lexical_form('http://www.w3.org/2001/XMLSchema#boolean',_,_)).
-known_error_(non_canonical_lexical_form('http://www.w3.org/2001/XMLSchema#dateTime',_,_)).
-known_error_(non_canonical_lexical_form('http://www.w3.org/2001/XMLSchema#decimal',_,_)).
-known_error_(non_canonical_lexical_form('http://www.w3.org/2001/XMLSchema#double',_,_)).
-known_error_(non_canonical_lexical_form('http://www.w3.org/2001/XMLSchema#float',_,_)).
-%known_error_(non_canonical_lexical_form('http://www.w3.org/2001/XMLSchema#int',_,_)).
-%known_error_(non_canonical_lexical_form('http://www.w3.org/2001/XMLSchema#integer',_,_)).
-known_error_(rdf(non_rdf_format(_Hash,_Content))).
-%known_error_(rdf(redefined_id(_Term))).
-known_error_(rdf(unexpected(_Tag, _Parser))). % TBD: Enable this after looking into RSS.
-%known_error_(rdf(unparsed(_Dom))).
-known_error_(rdf(unsupported_format(_MediaType,_Content))).
-known_error_(sgml(sgml_parser(_Parser),_File,_Line,_Msg)).
-known_error_(unclear_encoding(_Encodings,_Encoding)).
+print_errors :-
+  forall(
+    error(_-Error),
+    (   known_error_(Error, Flag)
+    ->  flag(Flag, N, N+1)
+    ;   format("~w\n", [Error])
+    )
+  ),
+  format("---\n"),
+  forall(
+    known_error_(_, Flag),
+    (
+      flag(Flag, N, N),
+      format("~D\t~a\n", [N,Flag])
+    )
+  ).
 
 
 
@@ -142,6 +106,86 @@ print_status :-
     N
   ),
   format("~D\n", [N]).
+
+
+
+%! unknown_error(-Error:pair(atom,compound)) is nondet.
+
+unknown_error(Error) :-
+  error(Error),
+  Error = _-Error0,
+  \+ known_error_(Error0, _).
+
+known_error_(error(archive_error(2, 'Missing type keyword in mtree specification'),_), mtree).
+known_error_(error(domain_error(http_encoding,identity),_), http_encoding).
+known_error_(error(domain_error(set_cookie,_Value),_), http_cookie).
+%%%%known_error_(error(domain_error(url,_),_)). % TBD: ?
+known_error_(error(existence_error(turtle_prefix,_Prefix),_Stream), ttl_prefix).
+known_error_(error(http_status(400,_Msg),_Uri), http_client_bad_request).
+known_error_(error(http_status(401,_Msg),_Uri), http_client_unauthorized).
+known_error_(error(http_status(403,_Msg),_Uri), http_client_forbidden).
+known_error_(error(http_status(404,_Msg),_Uri), http_client_not_found).
+known_error_(error(http_status(406,_Msg),_Uri), http_client_not_acceptable).
+known_error_(error(http_status(410,_Msg),_Uri), http_client_gone).
+known_error_(error(http_status(500,_Msg),_Uri), http_server_internal_error).
+known_error_(error(http_status(502,_Msg),_Uri), http_server_bad_gateway).
+known_error_(error(http_status(503,_Msg),_Uri), http_server_unavailable).
+%%%%known_error_(error(io_error(read,_Stream),_Context)). % TBD: ?
+known_error_(io_warning(_Stream,'Illegal UTF-8 continuation'), illegal_utf8).
+known_error_(error(socket_error('Connection refused'),_), connection_refused).
+known_error_(error(socket_error('Connection reset by peer'),_), connection_reset).
+known_error_(error(socket_error('Connection timed out'),_), connection_timed_out).
+known_error_(error(socket_error('Host not found'),_), host_not_found).
+known_error_(error(socket_error('No Data'),_), no_data).
+known_error_(error(socket_error('No Recovery'),_), no_recovery).
+known_error_(error(socket_error('No route to host'),_), no_route_to_host).
+known_error_(error(syntax_error('End of statement expected'),_Stream), eos_expected).
+known_error_(error(syntax_error('EOF in string'),_Stream), eof_in_string).
+known_error_(error(syntax_error('Expected ":"'),_Stream), expected_colon).
+known_error_(error(syntax_error(http_paramter(_Param)),_), http_parameter).
+known_error_(error(syntax_error('Illegal character in uriref'),_Stream), illegal_char_uriref).
+known_error_(error(syntax_error('Illegal control character in uriref'),_Stream), illegal_control_char_uriref).
+known_error_(error(syntax_error('illegal escape'),_Stream), illegal_escape).
+known_error_(error(syntax_error('Illegal IRIREF'),_Stream), illegal_iriref).
+known_error_(error(syntax_error('PN_PREFIX expected'),_Stream), pn_prefix_expected).
+known_error_(error(syntax_error('predicate expected'),_Stream), predicate_expected).
+known_error_(error(syntax_error('subject expected'),_Stream), subject_expected).
+known_error_(error(syntax_error('subject not followed by whitespace'),_Stream), subject_whitespace).
+known_error_(error(timeout_error(read,_Stream),_Context), timeout).
+known_error_(http(max_redirect(_N,_Uris)), http_max_redirect).
+known_error_(http(no_content_type,_Uri), http_no_content_type).
+known_error_(http(redirect_loop(_Uri)), http_redirect_loop).
+known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#date',_), date).
+known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#dateTime',_), dateTime).
+known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#decimal',_), decimal).
+known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#double',_), double).
+known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#float',_), float).
+%known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#gDay',_)).
+%known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#gMonth',_)).
+known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#gMonthDay',_), gMonthDay).
+known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#gYear',_), gYear).
+known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#int',_), int).
+known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#integer',_), integer).
+known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#nonNegativeInteger',_), nonNegativeInteger).
+known_error_(incorrect_lexical_form('http://www.w3.org/2001/XMLSchema#time',_), time).
+known_error_(missing_language_tag(_LTag), missing_ltag).
+known_error_(non_canonical_language_tag(_LTag), noncan_langString).
+%known_error_(no_image(_Url)).
+known_error_(non_canonical_lexical_form('http://www.w3.org/2001/XMLSchema#boolean',_,_), noncan_boolean).
+known_error_(non_canonical_lexical_form('http://www.w3.org/2001/XMLSchema#dateTime',_,_), noncan_dateTime).
+known_error_(non_canonical_lexical_form('http://www.w3.org/2001/XMLSchema#decimal',_,_), noncan_decimal).
+known_error_(non_canonical_lexical_form('http://www.w3.org/2001/XMLSchema#double',_,_), noncan_double).
+known_error_(non_canonical_lexical_form('http://www.w3.org/2001/XMLSchema#float',_,_), noncan_float).
+known_error_(non_canonical_lexical_form('http://www.w3.org/2001/XMLSchema#int',_,_), noncan_int).
+%known_error_(non_canonical_lexical_form('http://www.w3.org/2001/XMLSchema#integer',_,_)).
+known_error_(rdf(non_rdf_format(_Hash,_Content)), non_rdf).
+known_error_(rdf(redefined_id(_Term)), redefined_id).
+known_error_(rdf(unexpected(_Tag, _Parser)), rdfxml_tag). % TBD: Enable this after looking into RSS.
+known_error_(rdf(unparsed(_Dom)), rdfxml_unparsed).
+known_error_(rdf(unsupported_format(_MediaType,_Content)), rdf_support).
+known_error_(sgml(sgml_parser(_Parser),_File,_Line,_Msg), rdfxml_sgml).
+known_error_(unclear_encoding(_Encodings,_Encoding), stream_encoding).
+known_error_(unsupported_license(_License), license).
 
 
 
