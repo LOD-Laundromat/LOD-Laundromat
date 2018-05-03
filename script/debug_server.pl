afc1bda971bf7f7bd266c0dcd78786112387b856
Wouter Beek <wouter@triply.cc>
2018-05-03 20:13:59 +0200

    FIXED: Debug statement must appear before initialization.

--- script/debug_server.pl
+++ script/debug_server.pl
@@ -1,10 +1,10 @@
 :- use_module(library(debug)).
 :- use_module(library(thread_ext)).
 
+:- debug(ll(offline)).
+
 :- use_module(library(ll/ll_download)).
 :- use_module(library(ll/ll_init)).
-
-:- debug(ll(offline)).
 
 debug_uri(Uri) :-
   ll_download(Uri).
