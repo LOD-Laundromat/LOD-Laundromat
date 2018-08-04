cfcdce98853b042dfd61ed28bfa24e8836e00d84
Wouter Beek <wouter@triply.cc>
2018-08-04 21:11:15 +0200

    FIXED: Do not set success condition (only failure condition).

--- prolog/ll/ll_download.pl
+++ prolog/ll/ll_download.pl
@@ -70,11 +70,7 @@ download_url(Hash, Uri) :-
 
 download_stream(Hash, Uri, Out, MediaType, Status, FinalUri) :-
   findall(RdfMediaType, rdf_media_type(RdfMediaType), RdfMediaTypes),
-  http_open2(
-    Uri,
-    In,
-    [accept(RdfMediaTypes),failure(_),metadata(HttpMetas),success(_)]
-  ),
+  http_open2(Uri, In, [accept(RdfMediaTypes),failure(_),metadata(HttpMetas)]),
   ignore(http_metadata_content_type(HttpMetas, MediaType)),
   write_meta_http(Hash, HttpMetas),
   http_metadata_final_uri(HttpMetas, FinalUri),
